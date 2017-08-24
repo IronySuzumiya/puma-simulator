03b97e7ee4c48a154e4d49955eebf70e17daede9
Aayush Ankit <ankitaay@dpe-emulate.labs.hpecorp.net>
2017-08-24 10:07:32 -0700

    Compiled LSTM results

--- src/dpe.py
+++ src/dpe.py
@@ -130,15 +130,15 @@ metric_dict = get_hw_stats (fid, node_dut, cycle)
 fid.close ()
 print 'Success: Hadrware results compiled!!'
 
-## Compare with GPU results (dynamic energy only)
-#dpe_energy_l1 = metric_dict['total_energy'] * 64* 112 * 112
-#print (str (dpe_energy_l1) + ' joules')
-#
-#gpu_leak = 16 #watt
-#gpu_tot = 42
-#gpu_time_l1 = 2.438
-#dpe_time_l1 = metric_dict['time'] * 64*112*112
-#gpu_energy_l1 = (gpu_tot)*gpu_time_l1
-#
-#print ('energyX', str (gpu_energy_l1/dpe_energy_l1))
-#print ('timeX', str (gpu_time_l1/dpe_time_l1))
+# Compare with GPU results (dynamic energy only)
+dpe_energy_l1 = metric_dict['total_energy']
+print (str (dpe_energy_l1) + ' joules')
+
+gpu_leak = 16 #watt
+gpu_tot = 51
+gpu_time_l1 = 0.629 / 1000
+dpe_time_l1 = metric_dict['time']
+gpu_energy_l1 = (gpu_tot)*gpu_time_l1
+
+print ('energyX', str (gpu_energy_l1/dpe_energy_l1))
+print ('timeX', str (gpu_time_l1/dpe_time_l1))
