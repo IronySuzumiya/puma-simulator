466d95c70c348afab8402d4b8fce3fe7200636cf
Fernando Felix <fernandofelix@copin.ufcg.edu.br>
2018-11-28 16:37:53 -0300

    add simple precompiled model

---
+++ test/testasm/simple/tile0/simple-tile0.puma.py
@@ -0,0 +1,17 @@
+
+import sys, os
+import numpy as np
+import math
+sys.path.insert (0, '/home/fernandofelix/Workspace/dpe/training/dpe_emulate/include/')
+sys.path.insert (0, '/home/fernandofelix/Workspace/dpe/training/dpe_emulate/src/')
+from data_convert import *
+from instrn_proto import *
+from tile_instrn_proto import *
+dict_temp = {}
+dict_list = []
+i_temp = i_send(mem_addr=0, vtile_id=0, send_width=5, target_addr=2, vec=1)
+dict_list.append(i_temp.copy())
+i_temp = i_halt()
+dict_list.append(i_temp.copy())
+filename = 'simple/tile0/tile_imem.npy'
+np.save(filename, dict_list)
---
+++ test/testasm/simple/tile2/simple-tile2-core0.puma.py
@@ -0,0 +1,26 @@
+
+import sys, os
+import numpy as np
+import math
+sys.path.insert (0, '/home/fernandofelix/Workspace/dpe/training/dpe_emulate/include/')
+sys.path.insert (0, '/home/fernandofelix/Workspace/dpe/training/dpe_emulate/src/')
+from data_convert import *
+from instrn_proto import *
+from tile_instrn_proto import *
+dict_temp = {}
+dict_list = []
+i_temp = i_set(d1=257, imm=0, vec=1)
+dict_list.append(i_temp.copy())
+i_temp = i_load(d1=0, r1=257, load_width=5, vec=1)
+dict_list.append(i_temp.copy())
+#i_temp = i_mvm(0b01)
+i_temp = i_mvm(['100', '001'])
+dict_list.append(i_temp.copy())
+i_temp = i_set(d1=257, imm=5, vec=1)
+dict_list.append(i_temp.copy())
+i_temp = i_store(d1=257, r1=0, counter=1, store_width=5, vec=1)
+dict_list.append(i_temp.copy())
+i_temp = i_hlt()
+dict_list.append(i_temp.copy())
+filename = 'simple/tile2/core_imem0.npy'
+np.save(filename, dict_list)
